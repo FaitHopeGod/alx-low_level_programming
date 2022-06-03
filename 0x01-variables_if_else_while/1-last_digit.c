34787ac5f10f228811866c5647d8f19d664c0977
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-03 10:41:20 -0700

    LAST_DIGIT

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,12 +4,11 @@
 /**
  * main - Entry point
  *
- * Retun: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n, last_digit;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
