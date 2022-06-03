f4a7dca75403a52822447098b1ff8b2416db5f67
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-03 12:56:19 -0700

    9-0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int x;
+	for (x = 9; x >= 0; x--)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}
