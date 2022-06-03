362217d25a425570f16d9cbfc6cd5bf11cae69ed
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-03 11:46:05 -0700

    PUTCHAR

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,10 +5,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	for(int x = 'a'; x <= 'z'; x++)
+{	int x;
+	for (x = 'a'; x <= 'z'; x++)
 		putchar(x);
-	for (int x = 'A'; x <='Z'; x++)
+	for (x = 'A'; x <= 'Z'; x++)
 		putchar(x);
 	putchar('\n');
 	return (0);
