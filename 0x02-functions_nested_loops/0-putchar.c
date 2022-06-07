5a1598f64ba20be0d0c5dfe7c97233a89a0ded17
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-07 20:49:11 +0000

    Ex_0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "main.c"
-/*
+/**
  * main - main function to print _putchar
- *
- * Description: printing _putchar
  *
  * Return: Always return (0)
  */
+
 int main(void)
 {
 	_putchar("_putchar\n");
