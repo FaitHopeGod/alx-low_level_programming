ce7963c3ef8f16040e386f884d5b53ea2134f473
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-07 21:04:10 +0000

    Ex_0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.c"
+#include "main.h"
 /**
  * main - main function to print _putchar
  *
@@ -8,6 +8,19 @@
 
 int main(void)
 {
-	_putchar("_putchar\n");
+	char pu[8];
+	pu[0] = '_';
+	pu[1] = 'p';
+	pu[2] = 'u';
+	pu[3] = 't';
+	pu[4] = 'c';
+	pu[5] = 'h';
+	pu[6] = 'a';
+	pu[7] = 'r';
+	for(int i = 0; i < 8; i++)
+			{
+			_putchar(pu[i]);
+			}
+
 	return (0);
 }
