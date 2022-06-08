3818af3bfcb23cda86bc3cf491562a85944f604a
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-08 03:18:40 -0700

    Ex_4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - identify lowercase charachters
+ *
+ *@c: integer parameter
+ *
+ * Return: 1 if it is letter, lowercase or uppercase else 0
+ */
+
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else if(c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
