66011a154eb3c8bd47cd3a7bfd7d06c98dadfc90
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-08 10:39:23 -0700

    Ex_10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_add - computes the sum of two an integer
+ *
+ *@a: integer parameter
+ *@b: integer parameter
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return a+b;
+
+}
