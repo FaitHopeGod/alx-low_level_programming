370078ea9024e295a6e31b397f9623f9a40e2ed5
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-08 14:05:27 -0700

    Ex_11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,14 @@ void print_to_98(int n)
 		}
 		else if (n < 0)
 		{
-			if (n > -100)
+			if (n < 0 && n > -10)
+			{
+				_putchar((-1 * n) % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+
+			}
+			else if (n > -100)
 			{
 				_putchar('-');
 				_putchar((-1 * n) / 10 + '0');
