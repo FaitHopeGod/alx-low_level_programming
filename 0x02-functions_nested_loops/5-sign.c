b9017193587fe67aeb0f3579bd622f0df3a0fbb8
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-08 03:51:01 -0700

    Ex_5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_sign - identify Postive & Negative number and print sign
+ *
+ *@c: integer parameter
+ *
+ * Return: 1 if it is Positive, -1 if it is Negative, else 0
+ */
+
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+')
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0')
+		return (0);
+	}
+	else
+	{
+		_putchar('-')
+		return (-1);
+	}
+}
