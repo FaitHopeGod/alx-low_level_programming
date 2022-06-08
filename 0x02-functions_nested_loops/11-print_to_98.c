382ec9d144d6dd3384b97f93499e3528dda635e4
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-08 12:47:24 -0700

    Ex_11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 
 void print_to_98(int n)
 {
-	while (n < 98)
+	while (n <= 98)
 	{
 		if (n < 10)
 		{
@@ -20,6 +20,7 @@ void print_to_98(int n)
 		{
 			_putchar(n / 10 + '0');
 			_putchar(n % 10 + '0');
+			if (n != 98)
 			_putchar(',');
 		}
 		n++;
