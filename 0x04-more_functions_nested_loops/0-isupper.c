02ab369f8ca8a809f091f7390322054ab0383440
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-09 18:29:36 +0000

    Ex_0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _isupper - Differentiate Upper Cases
+ *
+ * Return: Integer number
+ *
+ * @c: integer parameter
+ */
+
+int _isupper(int c)
+{
+	int r;
+
+	if (c >= 65 && c <= 90)
+		r = 1;
+	else
+		r = 0;
+
+	return (r);
+}
