1b429c586a9b37a68d3979b77046152e59dd213b
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-09 23:04:55 +0000

    Ex_4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 /**
  *print_most_numbers - Display 0-9 without 2 and 4
  *
@@ -12,11 +13,13 @@ void print_most_numbers(void)
 	int i;
 
 	i = 0;
-	while (i != 2 && i != 4 && i < 10)
+	while (i < 10)
 	{
-		_putchar(i + '0');
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 		i++;
 	}
-
 	_putchar ('\n');
 }
