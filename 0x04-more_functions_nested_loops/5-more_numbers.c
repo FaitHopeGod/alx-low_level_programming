5109df464e2b951b755937875a36bd20150443e2
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-09 23:25:21 +0000

    Ex_5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ *most_numbers - Display 0-14 x10
+ *
+ * Return: Nothing
+ *
+ */
+
+void most_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 15; j++)
+		{
+			if(j > 9)
+				_putchar(j/10 + '0');
+			_putchar(j%10 + '0');
+		}
+	_putchar ('\n');
+	}
+}
