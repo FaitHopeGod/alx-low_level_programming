95e987fe5035307ad5479f61d12993a999802c34
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-09 22:09:58 +0000

    Ex_3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - Display 0-9$
+ *
+ * Return: Nothing
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i=0;
+	while(i < 10)
+		_putchar(i + '0');
+	if (i == 10)
+		_putchar('$');
+}
