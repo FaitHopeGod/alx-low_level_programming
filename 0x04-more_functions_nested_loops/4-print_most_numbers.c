9f5021d6a269d4284bdc93108be236b2f8d79d75
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-09 22:44:34 +0000

    Ex_4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_most_numbers - Display 0-9 without 2 and 4
+ *
+ * Return: Nothing
+ *
+ */
+
+void print_most_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+		_putchar ('\n');
+	}
+}
