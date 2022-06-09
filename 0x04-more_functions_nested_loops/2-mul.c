ff3e0da732c320d04d390920e23d29903d20b436
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-09 21:59:49 +0000

    Ex_2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Differentiate Upper Cases
+ *
+ * Return: Integer number
+ *
+ * @a: integer parameter
+ * @b: integer parameter
+ */
+
+int mul(int a, int b)
+{
+	long int res;
+
+	res = a * b;
+	return (res);
+}
