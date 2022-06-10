1a75c5d640af39fc4c3e62671ede328412901fa7
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-10 16:34:23 +0000

    Ex_5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,11 @@
 
 void more_numbers(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i, j;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = 0; j < 15; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j > 9)
 				_putchar(j / 10 + '0');
