274d968addebff2789642d0b6c1d12d76aa816cb
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-10 18:04:56 +0000

    Ex_9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- * print_fizz_buzz - Display number with fizz buzz order
- * return: Always zero 0
+ *
+ *
+ * Return: Always zero 0
 */
-void print_fizz_buzz(void)
+int main(void)
 {
 	int i;
-	for (i=0; i<=100; i++)
+
+	for (i = 0; i <= 100; i++)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz");
@@ -16,15 +18,10 @@ void print_fizz_buzz(void)
 		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%d",i);
+			printf("%d", i);
 		printf(" ");
 	}
 
 
-}
-
-int main(void)
-{
-	print_fizz_buzz();
 	return (0);
 }
