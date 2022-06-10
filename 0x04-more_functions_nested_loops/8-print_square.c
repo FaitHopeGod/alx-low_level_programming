b1bb8655991665ca25d3b9a40c92d2b4bdfa2e49
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-10 16:45:57 +0000

    Ex_8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,11 +10,13 @@
 
 void print_square(int size)
 {
-	if(size == 0)
+	int i, j;
+
+	if (size == 0)
 		_putchar ('\n');
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (int j = 0; j < size; j++)
+		for (j = 0; j < size; j++)
 			_putchar('#');
 		_putchar ('\n');
 	}
