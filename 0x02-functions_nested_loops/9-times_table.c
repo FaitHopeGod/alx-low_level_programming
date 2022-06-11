795e0a75d942242003ba286c1f20993732e42e21
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-11 14:03:35 -0700

    Ex_9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,6 +9,7 @@ void times_table(void)
 	int i, j;
 
 	for (i = 0; i <= 9; i++)
+	{
 		for (j = 0; j <= 9; j++)
 		{
 			if (i != 9 || j != 9)
@@ -31,4 +32,5 @@ void times_table(void)
 			}
 		}
 		_putchar('\n');
+	}
 }
