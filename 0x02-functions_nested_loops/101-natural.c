4d4654285b619b161a44ab478c57e69ae5be9565
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-11 23:24:59 +0000

    Ex_ADV12

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+/**
+ * main - printing n's time table
+ *
+ * Return: No return it is void.
+ */
+int main (void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);		
+}
