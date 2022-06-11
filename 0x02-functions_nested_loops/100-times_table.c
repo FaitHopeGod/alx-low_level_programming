24e5b59774b8c2f1c3d800ceb1c28bdc85f40f8a
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-11 22:10:16 +0000

    Ex_ADV11

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * print_times_table - printing n's time table
+ *
+ * @n: integer parameter from Entry point
+ *
+ * Return: No return it is void.
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (n >= 0 && n <= 15)
+			{
+				if (j == 0)
+					_putchar(i * j + '0');
+				else
+				{
+					if (i * j <= 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(i * j + '0');
+					}
+					else if (i * j > 9 && i * j <= 99)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((i * j) / 10 + '0');
+						_putchar((i * j) % 10 + '0');
+					}
+					else
+					{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((i / j) % 100 + '0');
+					_putchar(((i / j) % 100) % 10 + '0');
+					_putchar((i / j) / 10 + '0');
+
+					}
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
