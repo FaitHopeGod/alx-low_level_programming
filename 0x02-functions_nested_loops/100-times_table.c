8e347002c5e4a58deffa2d79005b599ccaebcf64
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-11 23:07:44 +0000

    Ex_ADV11

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@
 void print_times_table(int n)
 {
 	int i, j;
-	
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -20,26 +20,22 @@ void print_times_table(int n)
 					_putchar(i * j + '0');
 				else
 				{
+					_putchar(',');
+					_putchar(' ');
 					if (i * j <= 9)
 					{
-						_putchar(',');
-						_putchar(' ');
 						_putchar(' ');
 						_putchar(' ');
 						_putchar(i * j + '0');
 					}
 					else if (i * j > 9 && i * j <= 99)
 					{
-						_putchar(',');
-						_putchar(' ');
 						_putchar(' ');
 						_putchar((i * j) / 10 + '0');
 						_putchar((i * j) % 10 + '0');
 					}
 					else
 					{
-					_putchar(',');
-					_putchar(' ');
 					_putchar((i * j) / 100 + '0');
 					_putchar(((i * j) % 100) / 10 + '0');
 					_putchar(((i * j) % 100) % 10 + '0');
