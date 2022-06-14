115c7e00b333c5485244a80488eb5af2c1c0d447
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-14 23:11:33 +0000

    Task_#9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy- copy from source to the destination
+ *
+ * @dest: character pointer
+ * @src: character pointer
+ *
+ * Return: character
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+	
+	i = 0;
+	j = 0;
+
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	for ( ; j < i ; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+
+}
