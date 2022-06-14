21d538725c6a06fc0e3532edbad1fd53b56d5fbb
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-14 19:45:27 +0000

    Task_#4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev- print a string in reverse order
+ *
+ * @str: character pointer
+ */
+void print_rev(char *s)
+{	
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		i++;
+	i= i-1;
+	while (i >= 0)
+	{
+		printf("%c", *(s + i));
+		i--;
+	}
+	printf("\n");
+}
