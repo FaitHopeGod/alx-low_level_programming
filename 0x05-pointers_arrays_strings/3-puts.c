071ea4a1feb84d8113da1b9f2242181b10c56f0e
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-14 18:17:04 +0000

    Task#3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts- print a string followed by a new line to stdout
+ *
+ * @str: character pointer
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
