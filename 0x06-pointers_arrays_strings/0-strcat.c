2ed7ee814d0adf8bef45ded5f86ffa00057f8713
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-16 03:59:54 -0700

    Task_#0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,23 +6,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, d_sz, s_sz;
+	int i, d_sz;
 	
-	d_sz = 0;
-	s_sz = 0;
 	i = 0;
-
-	while (*(dest + i) != '\0')
+	d_sz = 0;
+	
+	while (*(dest + d_sz) != '\0')
 		d_sz++;
-	while (*(src + i) != '\0')
-		s_sz++;
 
-	while (d_sz < (d_sz + s_sz) && d_sz < (d_sz + s_sz))
+	while (src[i] != '\0')
 	{
-		*(dest + des_sz) = *(src + i);
+		dest[d_sz] = src[i];
 		d_sz++;
 		i++;
 	}
-	return (*dest);
+	dest[d_sz] = '\0';
+	return (dest);
 
 }
