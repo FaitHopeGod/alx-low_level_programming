959c8af6c86feffc821af580f0bd07bf7c086053
Tesfahunegn <TesfaithM@gmail.com>
2022-06-20 13:44:10 +0000

    Task_#0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset- fills the first n bytes of the memory area
+ *
+ * @s:
+ * @b:
+ * @n:
+ *
+ * Return: character
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This part of repository contain example and exercise regarding 0x07. C - Even more pointers, arrays and strings
