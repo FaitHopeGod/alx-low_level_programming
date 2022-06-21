b636ae5f0ea980665412c6b8e0e3555e0cac5934
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-21 20:10:48 +0000

    Task#0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,16 +2,23 @@
 /**
  * _memset- fills the first n bytes of the memory area
  *
- * @s:
- * @b:
- * @n:
+ * @s:source
+ * @b:constant byte
+ * @n:buffer size
  *
  * Return: character
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
-
+	i = 0;
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
 }
 
