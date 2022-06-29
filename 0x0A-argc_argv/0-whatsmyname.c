051eb7733b164090e2e7fe1dc811972b16bf31e1
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-28 23:57:45 -0700

    task_#0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,15 @@
 /**
  * main - main function to print program name
  *
- * argc : integer argument size
+ * @argc : integer argument size
  *
- * argv : array of arguments
+ * @argv : array of arguments
  *
- * Return : zero(0)
+ * Return: zero(0)
  */
 
-int main (int argc, char* argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n",argv[0]);
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
