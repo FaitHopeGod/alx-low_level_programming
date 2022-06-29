2c1ddc080d34a29ba79dca3c1574ba2dcc2e0d2e
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-29 00:03:59 -0700

    task_#1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function to print program parameter size
+ *
+ * @argc : integer argument size
+ *
+ * @argv : array of arguments
+ *
+ * Return: zero(0)
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
