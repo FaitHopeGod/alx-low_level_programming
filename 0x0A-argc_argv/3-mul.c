845efd5472b9acf72d765a68a9e8a9a415d3461b
Tesfahunegn Mengistu <TesfaitM@gmail.com>
2022-06-29 01:24:34 -0700

    task_#3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function to print the multiplication of arguments
+ *
+ * @argc : integer argument size
+ *
+ * @argv : array of arguments
+ *
+ * Return: zero(0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, res = 1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+			res = res * atoi(argv[i]);
+		printf("%d\n", res);
+		return (res);
+	}
+}
